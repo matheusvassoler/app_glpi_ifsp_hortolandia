82bccd253898d96c1fa658651d5e57339d4d5ff9
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2021-02-01 22:44:06 -0300

    Correção de Detekt e Ktlint em arquivos do projeto

--- app/src/test/java/com/glpi/ifsp/hortolandia/ExampleUnitTest.kt
+++ app/src/test/java/com/glpi/ifsp/hortolandia/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.glpi.ifsp.hortolandia
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
