eb9afa6ea6d17bd20ad344cf1ad4ee7a88806c73
Matheus Vassoler <mvassoler@ciandt.com>
2021-06-16 11:45:35 -0300

    Adiciona o LogoutRepository para ser gerenciado pelo Koin

--- app/src/main/java/com/glpi/ifsp/hortolandia/infrastructure/di/RepositoryModule.kt
+++ app/src/main/java/com/glpi/ifsp/hortolandia/infrastructure/di/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.glpi.ifsp.hortolandia.infrastructure.di
 
 import com.glpi.ifsp.hortolandia.data.repository.login.LoginRemoteRepository
 import com.glpi.ifsp.hortolandia.data.repository.login.LoginRepository
+import com.glpi.ifsp.hortolandia.data.repository.logout.LogoutRemoteRepository
+import com.glpi.ifsp.hortolandia.data.repository.logout.LogoutRepository
 import com.glpi.ifsp.hortolandia.data.repository.session.SessionLocalRepository
 import com.glpi.ifsp.hortolandia.data.repository.session.SessionRepository
 import org.koin.dsl.module
@@ -9,4 +11,5 @@ import org.koin.dsl.module
 val repositoryModule = module {
     single<LoginRepository> { LoginRemoteRepository(get()) }
     single<SessionRepository> { SessionLocalRepository(get()) }
+    single<LogoutRepository> { LogoutRemoteRepository(get()) }
 }
