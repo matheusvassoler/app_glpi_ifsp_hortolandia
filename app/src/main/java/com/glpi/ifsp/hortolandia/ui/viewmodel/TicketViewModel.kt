a01a6ddee042f467fc8665c963b3c3095a2998de
Matheus Vassoler <mvassoler@ciandt.com>
2021-07-10 16:28:43 -0300

    [TIF-51] Criação do ViewModel - Implementação do tratamento de exception caso o session token esteja nulo

--- app/src/main/java/com/glpi/ifsp/hortolandia/ui/viewmodel/TicketViewModel.kt
+++ app/src/main/java/com/glpi/ifsp/hortolandia/ui/viewmodel/TicketViewModel.kt
@@ -1,11 +1,15 @@
 package com.glpi.ifsp.hortolandia.ui.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.glpi.ifsp.hortolandia.data.model.Ticket
 import com.glpi.ifsp.hortolandia.domain.GetTicketsUseCase
+import com.glpi.ifsp.hortolandia.infrastructure.exceptions.InternalErrorException
+import com.glpi.ifsp.hortolandia.ui.event.TicketEvent
+import com.hadilq.liveevent.LiveEvent
 import kotlinx.coroutines.flow.Flow
 
 class TicketViewModel(
@@ -16,7 +20,15 @@ class TicketViewModel(
     val ticketFlow: Flow<PagingData<Ticket>>
         get() = _ticketFlow
 
+    private var _event = LiveEvent<TicketEvent>()
+    val event: LiveData<TicketEvent>
+        get() = _event
+
     fun onStart() {
-        _ticketFlow = getTicketsUseCase().cachedIn(viewModelScope)
+        try {
+            _ticketFlow = getTicketsUseCase().cachedIn(viewModelScope)
+        } catch (e: InternalErrorException) {
+            _event.value = TicketEvent.ShowInternalError
+        }
     }
 }
--- app/src/test/java/com/glpi/ifsp/hortolandia/ui/viewmodel/TicketViewModelTest.kt
+++ app/src/test/java/com/glpi/ifsp/hortolandia/ui/viewmodel/TicketViewModelTest.kt
@@ -1,16 +1,20 @@
 package com.glpi.ifsp.hortolandia.ui.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.paging.AsyncPagingDataDiffer
 import androidx.paging.PagingData
 import com.glpi.ifsp.hortolandia.BaseUnitTest
 import com.glpi.ifsp.hortolandia.data.model.Ticket
 import com.glpi.ifsp.hortolandia.domain.GetTicketsUseCase
-import com.google.common.truth.Truth
+import com.glpi.ifsp.hortolandia.infrastructure.exceptions.InternalErrorException
+import com.glpi.ifsp.hortolandia.ui.event.TicketEvent
 import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.slot
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
@@ -23,6 +27,7 @@ import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
@@ -34,13 +39,26 @@ class TicketViewModelTest : BaseUnitTest() {
     @InjectMockKs
     private lateinit var viewModel: TicketViewModel
 
+    @RelaxedMockK
+    lateinit var eventObserver: Observer<TicketEvent>
+
+    private val eventSlot = slot<TicketEvent>()
+
+    private val eventList: ArrayList<TicketEvent> = arrayListOf()
+
     private val testDispatcher = TestCoroutineDispatcher()
     private val testScope = TestCoroutineScope(testDispatcher)
+
+    @get: Rule
+    val rule = InstantTaskExecutorRule()
 
     @Before
     override fun setup() {
         super.setup()
         Dispatchers.setMain(testDispatcher)
+        // must happen in this order so we can capture initial event
+        every { eventObserver.onChanged(capture(eventSlot)) } answers { eventList.add(eventSlot.captured) }
+        viewModel.event.observeForever(eventObserver)
     }
 
     @After
@@ -73,5 +91,16 @@ class TicketViewModelTest : BaseUnitTest() {
         }
         // THEN
         assertThat(list.snapshot().items).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun `onStart SHOULD emit ShowInternalError WHEN Session Token in shared preferences is null`() {
+        // GIVEN
+        every { getTicketsUseCase() } throws InternalErrorException()
+        // WHEN
+        viewModel.onStart()
+        // THEN
+        assertThat(eventList.size).isEqualTo(1)
+        assertThat(eventList[0]).isInstanceOf(TicketEvent.ShowInternalError::class.java)
     }
 }
