6fbaccb5f9af0b7aa7786d58c0ceff86fcaf8d44
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2021-10-30 00:08:01 -0300

    [TIF-76] Implementação da leitura do QrCode

--- app/src/main/java/com/glpi/ifsp/hortolandia/ui/activity/OpenTicketActivity.kt
+++ app/src/main/java/com/glpi/ifsp/hortolandia/ui/activity/OpenTicketActivity.kt
@@ -1,10 +1,7 @@
 package com.glpi.ifsp.hortolandia.ui.activity
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
-import androidx.fragment.app.Fragment
 import com.glpi.ifsp.hortolandia.R
 import com.glpi.ifsp.hortolandia.databinding.ActivityOpenTicketBinding
 import com.glpi.ifsp.hortolandia.ui.fragment.OpenTicketTutorialFragment
@@ -19,37 +16,11 @@ class OpenTicketActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         attachFragmentToActivity()
-        setToolbar()
     }
 
     private fun attachFragmentToActivity() {
         supportFragmentManager.beginTransaction().apply {
-            add(R.id.activity_open_ticket_container, OpenTicketTutorialFragment(), "open_ticket_fragment")
+            replace(R.id.activity_open_ticket_container, OpenTicketTutorialFragment(), "open_ticket_fragment")
         }.commit()
-    }
-
-    private fun setToolbar() {
-        binding.activityOpenTicketToolbar.toolbarTitle.text =
-            getString(R.string.open_ticket_toolbar_title)
-        binding.activityOpenTicketToolbar.toolbar.navigationIcon =
-            ResourcesCompat.getDrawable(resources, R.drawable.back_button_icon, null)
-        binding.activityOpenTicketToolbar.toolbar.setNavigationOnClickListener {
-            goToHomeActivity()
-        }
-    }
-
-    private fun goToHomeActivity() {
-        val myFragment = supportFragmentManager.findFragmentByTag("open_ticket_fragment")
-        if (checkIfOpenTicketTutorialFragmentIsVisible(myFragment)) {
-            startActivity(Intent(this, HomeActivity::class.java))
-        }
-    }
-
-    private fun checkIfOpenTicketTutorialFragmentIsVisible(myFragment: Fragment?) =
-        myFragment != null && myFragment.isVisible
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        goToHomeActivity()
     }
 }
--- app/src/main/java/com/glpi/ifsp/hortolandia/ui/fragment/OpenTicketTutorialFragment.kt
+++ app/src/main/java/com/glpi/ifsp/hortolandia/ui/fragment/OpenTicketTutorialFragment.kt
@@ -1,11 +1,16 @@
 package com.glpi.ifsp.hortolandia.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
+import com.glpi.ifsp.hortolandia.R
 import com.glpi.ifsp.hortolandia.databinding.FragmentOpenTicketTutorialBinding
+import com.glpi.ifsp.hortolandia.ui.activity.HomeActivity
 
 class OpenTicketTutorialFragment : Fragment() {
 
@@ -19,5 +24,53 @@ class OpenTicketTutorialFragment : Fragment() {
     ): View {
         _binding = FragmentOpenTicketTutorialBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setToolbar()
+        setListenerForCardView()
+        setCallbackForOnBackPressed()
+    }
+
+    private fun setListenerForCardView() {
+        binding.fragmentOpenTicketTutorialCardView.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()?.apply {
+                replace(R.id.activity_open_ticket_container, OpenTicketQrCodeScanFragment())
+                addToBackStack(null)
+            }?.commit()
+        }
+    }
+
+    private fun setCallbackForOnBackPressed() {
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            val intent = Intent(requireContext(), HomeActivity::class.java).apply {
+                flags =
+                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }
+            startActivity(intent)
+        }
+    }
+
+    private fun setToolbar() {
+        binding.fragmentOpenTicketTutorialToolbar.toolbarTitle.text =
+            getString(R.string.open_ticket_toolbar_title)
+        binding.fragmentOpenTicketTutorialToolbar.toolbar.navigationIcon =
+            ResourcesCompat.getDrawable(resources, R.drawable.back_button_icon, null)
+        binding.fragmentOpenTicketTutorialToolbar.toolbar.setNavigationOnClickListener {
+            goToHomeActivity()
+        }
+    }
+
+    private fun goToHomeActivity() {
+        val intent = Intent(requireContext(), HomeActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        startActivity(intent)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
