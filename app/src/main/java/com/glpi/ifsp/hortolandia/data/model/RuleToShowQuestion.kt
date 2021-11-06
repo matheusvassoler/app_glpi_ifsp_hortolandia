139aa64a3e360af64bc629b18c3f8b5ee13e2b15
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2021-11-06 11:38:31 -0300

    Atualiza o nome dos campos da dataclass RuleToShowQuestion.kt

--- app/src/main/java/com/glpi/ifsp/hortolandia/data/model/RuleToShowQuestion.kt
+++ app/src/main/java/com/glpi/ifsp/hortolandia/data/model/RuleToShowQuestion.kt
@@ -4,8 +4,8 @@ import com.google.gson.annotations.SerializedName
 
 data class RuleToShowQuestion(
     @SerializedName("id") val id: Int,
-    @SerializedName("items_id") val itemsId: Int,
-    @SerializedName("plugin_formcreator_questions_id") val pluginFormCreatorQuestionsId: Int,
-    @SerializedName("show_value") val showValue: String,
-    @SerializedName("show_condition") val showCondition: Int
+    @SerializedName("items_id") val questionIdThatDisappearsOrAppearsBasedOnACondition: Int,
+    @SerializedName("plugin_formcreator_questions_id") val questionIdThatControlsTheCondition: Int,
+    @SerializedName("show_value") val valueThatTriggersCondition: String,
+    @SerializedName("show_condition") val comparisonMethod: Int
 )
