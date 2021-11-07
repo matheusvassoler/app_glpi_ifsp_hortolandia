76477ddaf34116e05375d2160a23c4fd581889bd
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2021-11-07 16:46:36 -0300

    [TIF-88] Criação do model ValidationResponseSizeUI

---
+++ app/src/main/java/com/glpi/ifsp/hortolandia/ui/model/ValidationResponseSizeUI.kt
@@ -0,0 +1,8 @@
+package com.glpi.ifsp.hortolandia.ui.model
+
+data class ValidationResponseSizeUI(
+    val id: Int,
+    val questionIdThatHasValidation: Int,
+    val minimumNumberOfCharacters: Int,
+    val maximumNumberOfCharacters: Int
+)
