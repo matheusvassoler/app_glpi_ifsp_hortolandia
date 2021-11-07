c2ad2878f5b19f35a21ba79036ffc3ea5a8e3ba3
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2021-11-07 17:10:23 -0300

    [TIF-101] Construção do model LocationDataUI

---
+++ app/src/main/java/com/glpi/ifsp/hortolandia/ui/model/LocationUI.kt
@@ -0,0 +1,10 @@
+package com.glpi.ifsp.hortolandia.ui.model
+
+data class LocationUI(
+    val id: Int,
+    val name: String,
+    val parentLocation: Int,
+    val hierarchicalLevelOfLocation: Int,
+    val subLocationsId: List<Int>,
+    val completeName: String
+)
